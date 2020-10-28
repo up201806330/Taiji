c654f5ff4d905f6d36eb13a89daf2c42ac49b9c2
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2020-10-28 09:39:24 +0000

    Added intermediate and end board example

--- code/myBoard.pl
+++ code/myBoard.pl
@@ -2,7 +2,7 @@
 % translation(white, CHAR) :- CHAR = 'w'.
 % translation(black, CHAR) :- CHAR = 'b'.
 
-% board at the start
+% start state board
 initialBoard([
 [clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear],
 [clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear],
@@ -17,6 +17,35 @@ initialBoard([
 [clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear]
 ]).
 
+% intermediate state board
+intermediateBoard([
+[black,white,clear,clear,clear,clear,clear,clear,clear,clear,clear],
+[clear,black,clear,clear,clear,clear,clear,clear,clear,clear,clear],
+[clear,white,clear,clear,clear,clear,clear,clear,clear,clear,clear],
+[clear,clear,clear,clear,clear,clear,white,clear,clear,clear,clear],
+[clear,clear,black,white,white,black,black,clear,clear,clear,clear],
+[clear,black,black,white,white,black,black,white,clear,clear,clear],
+[clear,white,clear,clear,black,clear,white,black,clear,clear,clear],
+[clear,clear,clear,clear,white,clear,clear,clear,clear,clear,clear],
+[clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear],
+[clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear],
+[clear,clear,clear,clear,clear,clear,clear,clear,clear,clear,clear]
+]).
+
+% end state board
+endBoard([
+[black,white,white,white,black,white,clear,white,black,white,black],
+[clear,black,black,black,black,white,white,black,clear,white,black],
+[black,white,black,black,white,white,white,black,black,white,white],
+[white,white,white,white,black,black,white,black,white,clear,black],
+[white,black,black,white,white,black,black,black,white,black,white],
+[black,black,black,white,white,black,black,white,clear,white,black],
+[clear,white,white,black,black,clear,white,black,black,black,white],
+[black,white,black,white,white,black,black,clear,white,black,white],
+[black,white,clear,white,black,white,white,black,black,white,clear],
+[black,white,white,black,black,black,white,white,black,black,white],
+[clear,white,black,clear,white,clear,black,white,white,black,white]
+]).
 
 % custom write for each of the possible 
 write_char(clear) :- write('.').
