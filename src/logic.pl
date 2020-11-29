0d994c48a8d004a2d89fedfc1afac9f2613e4978
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-11-29 11:43:04 +0000

    Fixed those bugs

--- src/logic.pl
+++ src/logic.pl
@@ -44,7 +44,7 @@ turn(GameState, Gamemode, Player, Color) :-
     % game_over(GameState, Winner),
 
     % Get and write scores for black and white pieces
-    value(GameState, w, WhiteScore), value(GameState, b, BlackScore),
+    value(GameState, white, WhiteScore), value(GameState, black, BlackScore),
     nl, nl, write('White: '), write(WhiteScore), write(' | Black: '), write(BlackScore), nl, nl, 
     !,
 
@@ -55,11 +55,11 @@ turn(GameState, Gamemode, Player, Color) :-
     length(GameState, L),
     input_position(InputRow, InputCol, L),
     (
-        validate_position(InputRow, InputCol, ValidatedRow, ValidatedCol) -> true;
+        validate_position(InputRow, InputCol, L, ValidatedRow, ValidatedCol) -> true;
 
         nl, write('Invalid Taijitu position!'), nl,                % "Else statement"
         enter_to_continue, nl,
-        clear_terminal, turn(GameState, Gamemode, Player, Color)
+        turn(GameState, Gamemode, Player, Color)
     ),
 
     % Get input from player to choose orientation of the taijitu
@@ -69,7 +69,7 @@ turn(GameState, Gamemode, Player, Color) :-
 
         nl, write('Invalid Taijitu orientation!'), nl,                % "Else statement"
         enter_to_continue, nl,
-        clear_terminal, turn(GameState, Gamemode, Player, Color)
+        turn(GameState, Gamemode, Player, Color)
     ),
     
     % Get coords of black piece of Taijitu, according to chosen orientation
@@ -82,15 +82,10 @@ turn(GameState, Gamemode, Player, Color) :-
         
         nl, write('Cant place piece there!'), nl,                % "Else statement"
         enter_to_continue, nl,
-        clear_terminal, turn(GameState, Gamemode, Player, Color)
+        turn(GameState, Gamemode, Player, Color)
     )
     .
 turn(_,_,_,_):- write('Error occured ; leaving').
-
-validate_orientation('1').
-validate_orientation('2').
-validate_orientation('3').
-validate_orientation('4').
 
 % Called if Move is valid, continues to next turn with new GameState
 % success_play(+NewGameState, +Gamemode, +Player, +Color, -NewPlayer, -NewColor)
@@ -136,40 +131,40 @@ black_move_part(Row, Col, '4', BlackRow, Col) :- % black on the top
 
 % ----------------------------------------------------------------
 % Converts row and col chars to their corresponding indexes
-convert_input_row('a', 1).
-convert_input_row('b', 2).
-convert_input_row('c', 3).
-convert_input_row('d', 4).
-convert_input_row('e', 5).
-convert_input_row('f', 6).
-convert_input_row('g', 7).
-convert_input_row('h', 8).
-convert_input_row('i', 9).
-convert_input_row('j', 10).
-convert_input_row('k', 11).
-convert_input_row('A', 1).
-convert_input_row('B', 2).
-convert_input_row('C', 3).
-convert_input_row('D', 4).
-convert_input_row('E', 5).
-convert_input_row('F', 6).
-convert_input_row('G', 7).
-convert_input_row('H', 8).
-convert_input_row('I', 9).
-convert_input_row('J', 10).
-convert_input_row('K', 11).
+convert_input_row('a', 0).
+convert_input_row('b', 1).
+convert_input_row('c', 2).
+convert_input_row('d', 3).
+convert_input_row('e', 4).
+convert_input_row('f', 5).
+convert_input_row('g', 6).
+convert_input_row('h', 7).
+convert_input_row('i', 8).
+convert_input_row('j', 9).
+convert_input_row('k', 10).
+convert_input_row('A', 0).
+convert_input_row('B', 1).
+convert_input_row('C', 2).
+convert_input_row('D', 3).
+convert_input_row('E', 4).
+convert_input_row('F', 5).
+convert_input_row('G', 6).
+convert_input_row('H', 7).
+convert_input_row('I', 8).
+convert_input_row('J', 9).
+convert_input_row('K', 10).
 
-convert_input_col('0', 1).
-convert_input_col('1', 2).
-convert_input_col('2', 3).
-convert_input_col('3', 4).
-convert_input_col('4', 5).
-convert_input_col('5', 6).
-convert_input_col('6', 7).
-convert_input_col('7', 8).
-convert_input_col('8', 9).
-convert_input_col('9', 10).
-convert_input_col('X', 11).
+convert_input_col('0', 0).
+convert_input_col('1', 1).
+convert_input_col('2', 2).
+convert_input_col('3', 3).
+convert_input_col('4', 4).
+convert_input_col('5', 5).
+convert_input_col('6', 6).
+convert_input_col('7', 7).
+convert_input_col('8', 8).
+convert_input_col('9', 9).
+convert_input_col('X', 10).
 % ----------------------------------------------------------------
 % Input
 
@@ -179,19 +174,26 @@ input_position(InputRow, InputCol, L) :-
     write('White Piece Row ( A - '), LastRowIndex is 64 + L, write_char(LastRowIndex), write('): '), get_char(InputRow), get_char(_), nl,
     write('White Piece Col ( 0 - '), LastColIndex is 47 + L, write_char(LastColIndex), write('): '), get_char(InputCol), get_char(_), nl.
 
-% Checks if inputted row and column are valid values and are inside bounds, returning the values converted to indexes in range [0, 11]
-% validate_position(+InputRow, +InputCol, -ConvertedRow, -ConvertedCol)
-validate_position(InputRow, InputCol, ConvertedRow, ConvertedCol) :-
+% Checks if inputted row and column are valid values and are inside bounds, returning the values converted to indexes in range [0, 10]
+% validate_position(+InputRow, +InputCol, +L, -ConvertedRow, -ConvertedCol)
+validate_position(InputRow, InputCol, L, ConvertedRow, ConvertedCol) :-
     convert_input_row(InputRow, ConvertedRow),
     convert_input_col(InputCol, ConvertedCol),
-    ConvertedRow @> 0, ConvertedRow @< 12,
-    ConvertedCol @> 0, ConvertedCol @< 12.
+    ConvertedRow >= 0, ConvertedRow < L,
+    ConvertedCol >= 0, ConvertedCol < L.
+
+% Checks if inputted orientation is a valid value
+% validate_orientation(Option)
+validate_orientation('1').
+validate_orientation('2').
+validate_orientation('3').
+validate_orientation('4').
 
 % Gets the option of the orientation of the Taijitu from the user
 % input_orientation(-Option)
 input_orientation(Option) :-
     show_orientations, nl,
-    get_char(Option).
+    get_char(Option), get_char(_).
 
 % ----------------------------------------------------------------
 % Score
@@ -230,10 +232,12 @@ algorithm(GameState, Color, [H|T], Accumulator, VisitedIn, Value):-
 % Returns lists with all indexes of square arrays of length L
 % indexes_from_length(+L -Output)
 indexes_from_length(L,Output):-
-    (L =:= 7 -> append([], [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(2,0),(2, 1),(2,2),(2,3),(2,4),(2,5),(2,6),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6)], Output);
+   (L =:= 3 -> append([], [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)], Output);
+    L =:= 7 -> append([], [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(2,0),(2, 1),(2,2),(2,3),(2,4),(2,5),(2,6),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6)], Output);
     L =:= 9 -> append([], [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(3,8),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),(7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8)], Output);
     L =:= 11 -> append([], [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),(0,9),(0,10),(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(1,10),(2,0),(2,1),(2,2),(2,3),(2,4),(2,5),(2,6),(2,7),(2,8),(2,9),(2,10),(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(3,7),(3,8),(3,9),(3,10),(4,0),(4,1),(4,2),(4,3),(4,4),(4,5),(4,6),(4,7),(4,8),(4,9),(4,10),(5,0),(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9),(5,10),(6,0),(6,1),(6,2),(6,3),(6,4),(6,5),(6,6),(6,7),(6,8),(6,9),(6,10),(7,0),(7,1),(7,2),(7,3),(7,4),(7,5),(7,6),(7,7),(7,8),(7,9),(7,10),(8,0),(8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8),(8,9),(8,10),(9,0),(9,1),(9,2),(9,3),(9,4),(9,5),(9,6),(9,7),(9,8),(9,9),(9,10),(10,0),(10,1),(10,2),(10,3),(10,4),(10,5),(10,6),(10,7),(10,8),(10,9),(10,10)], Output);
-    fail).
+    fail
+   ).
 
 value(GameState, Color, Value):-
     length(GameState, L),
